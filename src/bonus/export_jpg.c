f5bd3231fdac271de4afc6d76ff41887e9a5bf4a
Sylvie Forster <sforster@c1r1s5.42lausanne.ch>
2026-01-21 17:09:08 +0100

    small correction

--- src/bonus/export_jpg.c
+++ src/bonus/export_jpg.c
@@ -6,7 +6,7 @@
 /*   By: sforster <sforster@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/08 16:50:06 by syl               #+#    #+#             */
-/*   Updated: 2026/01/21 16:54:07 by sforster         ###   ########.fr       */
+/*   Updated: 2026/01/21 17:05:46 by sforster         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,40 +38,6 @@ static void	init_bmp_headers(t_bmp_header *h, t_bmp_info_header *i)
 	i->colors_used = 0;
 	i->important_colors = 0;
 }
-/*
-static void	write_pixels(FILE *file, t_pix ***pix)
-{
-	int				x;
-	int				y;
-	int				padding;
-	unsigned char	pad[3];
-
-	pad[0] = 0;
-	pad[1] = 0;
-	pad[2] = 0;
-	padding = (4 - ((WND_WIDTH * 3) % 4)) % 4;
-	y = 0;
-	while (y < WND_HEIGHT)
-	{
-		x = 0;
-		while (x < WND_WIDTH)
-		{
-			unsigned char	r;
-			unsigned char	g;
-			unsigned char	b;
-
-			b = (unsigned char)(pix[x][y]->color->b * 255.0);
-			g = (unsigned char)(pix[x][y]->color->g * 255.0);
-			r = (unsigned char)(pix[x][y]->color->r * 255.0);
-			fwrite(&b, 1, 1, file);
-			fwrite(&g, 1, 1, file);
-			fwrite(&r, 1, 1, file);
-			x++;
-		}
-		fwrite(pad, 1, padding, file);
-		y++;
-	}
-}*/
 
 static void	print_pix(FILE *file, t_pix ***pix, int x, int y)
 {
